34e1b04a27c18c5d620f9f82e010040f0e595747
Ayush Sachdeva <55956808+ayushsachdeva1@users.noreply.github.com>
2023-07-03 13:29:09 -0700

    Update train_global_features.py

--- train_global_features.py
+++ train_global_features.py
@@ -31,7 +31,7 @@ def main():
 
     train_batch_size = 128
     valid_batch_size = 128
-    n_epochs = 40
+    n_epochs = 100
 
     # loaders
     train_transforms = transforms.Compose([transforms.Resize(256), transforms.RandomCrop(224), transforms.RandomHorizontalFlip(),  transforms.ToTensor(), transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),])
